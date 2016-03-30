6350bac5b45ff003fbd3796cde641be8edbc1f0d
Yannick Pot <yannickpot@gmail.com>
2016-03-30 09:51:00 +0200

    added jumbotron and css

---
+++ public/MO/index.php
@@ -0,0 +1,13 @@
+<?php 
+include_once 'includes/header.php';
+?>
+
+<div class="jumbotron">
+	<h1>Hello, world!</h1>
+	<p>...</p>
+	<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+</div>
+
+<?php 
+include_once 'includes/footer.php';
+?>
