6ef6d1cd67019f416ada9bacbac6ae032ad86659
Yannick Pot <yannickpot@gmail.com>
2016-04-09 16:08:24 +0200

    added container row and 3 features

--- public/MO/index.php
+++ public/MO/index.php
@@ -2,8 +2,22 @@
 include_once 'includes/header.php';
 ?>
 
-<div class="jumbotron">
+<div id="jumbotron" class="jumbotron">
 	<h1>Amazing!</h1>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-4 col-xs-12">
+			Feature 1
+		</div>
+		<div class="col-md-4 col-xs-12">
+			Feature 2
+		</div>
+		<div class="col-md-4 col-xs-12">
+			Feature 3
+		</div>
+	</div>
 </div>
 
 <?php 
