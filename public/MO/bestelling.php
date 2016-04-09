8de443f5531458c66454b0c5822b2e8a6e9452ad
Yannick Pot <yannickpot@gmail.com>
2016-04-09 17:46:38 +0200

    added bestelling insert and select

--- public/MO/bestelling.php
+++ public/MO/bestelling.php
@@ -3,6 +3,14 @@
 
 <?php 
 try{
+	$stmt = $conn->prepare("INSERT INTO `cart_klant` (Naam, Email) VALUES (?, ?)");
+	$stmt->bindParam(1, $Naam);
+	$stmt->bindParam(1, $Email);
+
+	$Naam = $_POST['Naam'];
+	$Email = $_POST['Email'];
+	$stmt-execute();
+	$lastId = $conn->lastInsertId();
 
 } catch (PDOException $ex){
 	if( $database_config['debug'] ){
@@ -15,7 +23,39 @@ try{
 
 <div class="container">
 	<div class="row">
-		
+		<?php
+			session_start();
+			$total_order = 0;
+			if( isset($_SESSION['cart_content']) ){
+				$cart_array = explode(',', $_SESSION['cart_content']);
+
+				foreach ($cart_array as $item) {
+					$query = "SELECT * FROM `cart_producten` WHERE `Id`='". $item ."' ";
+					// Try to execute the SQL query
+					$query_result = $conn->query( $query );
+					// Return the result of the query
+					$product = $query_result->fetch(PDO::FETCH_ASSOC);
+					$total_order += $product['Prijs'];
+				}
+			}
+
+			try{
+				$stmt = $conn->prepare("INSERT INTO `cart_bestellingen` (totaal_prijs, klant_id) VALUES (?, ?)");
+				$stmt->bindParam(1, $totaal_prijs);
+				$stmt->bindParam(1, $klant_id);
+
+				$totaal_prijs = $total_order;
+				$klant_id = $lastId;
+				$stmt-execute();
+				$lastOrderId = $conn->lastInsertId();
+			}	catch (PDOException $ex){
+				if( $database_config['debug'] ){
+					$error = $ex->getMessage();
+					echo $error;
+				}
+			}
+		?>
+
 	</div>
 </div>
 
